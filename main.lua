091993865951ff56e7fc1e1c3238f988daaba7e6
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-04-05 13:40:03 -0400

    Fix badges

--- main.lua
+++ main.lua
@@ -21,4 +21,5 @@ TexturePack {
 }
 Malverk.badges.lttm_ame = function(self, card, badges)
     badges[#badges + 1] = create_badge('Artist: Ame', G.C.PURPLE, G.C.WHITE, 0.7)
+    badges[#badges + 1] = create_badge('Common', G.C.BLUE, nil, 1.2)
 end
