391ae90a4b833eb8dc1514f1c501016863a61e98
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-04-05 11:16:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shoot the Looks to the Moon
+
+A [Balatro](https://www.playbalatro.com/) mod that replaces Shoot the Moon with Looks to the Moon from [Rain World](https://rainworldgame.com/).
+
+Art by Ame (@toworou on Discord)
+
+# Installation
+
+1. This mod requires [Steamodded](https://github.com/Steamodded/smods) and [Malverk](https://github.com/Eremel/Malverk). Follow the instructions to install those if you haven't already.
+2. Clone this repository into your mods folder, or download it as a zip and place it there.
+3. Enjoy!
---
+++ main.lua
@@ -0,0 +1,24 @@
+AltTexture {
+    key = "lttm",
+    set = "Joker",
+    path = "Jokers.png",
+    keys = { "j_shoot_the_moon" },
+    loc_txt = { name = "Shoot the Looks to the Moon" },
+    localization = {
+        j_shoot_the_moon = {
+            name = "Looks to the Moon",
+            badge = "lttm_ame"
+        }
+    }
+}
+TexturePack {
+    key = "shootthelookstothemoon",
+    textures = { "shootthelookstothemoon_lttm" },
+    loc_txt = {
+        name = "Shoot the Looks to the Moon",
+        text = { "Replaces Shoot the Moon", "with Looks to the Moon" }
+    }
+}
+Malverk.badges.lttm_ame = function(self, card, badges)
+    badges[#badges + 1] = create_badge('Artist: Ame', G.C.PURPLE, G.C.WHITE, 0.7)
+end
