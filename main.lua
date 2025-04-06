b61eddc3c5cc8ea255bb3849ee13ff0f73bd8d00
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-04-05 21:29:34 -0400

    Echoes

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 AltTexture {
     key = "lttm",
     set = "Joker",
-    path = "Jokers.png",
+    path = "lttm.png",
     keys = { "j_shoot_the_moon" },
-    loc_txt = { name = "Shoot the Looks to the Moon" },
+    loc_txt = { name = "Looks to the Moon" },
     localization = {
         j_shoot_the_moon = {
             name = "Looks to the Moon",
@@ -11,9 +11,22 @@ AltTexture {
         }
     }
 }
+AltTexture {
+    key = "echo",
+    set = "Joker",
+    path = "echo.png",
+    keys = { "j_madness" },
+    loc_txt = { name = "Echoes" },
+    localization = {
+        j_madness = {
+            name = "Echoes",
+            badge = "lttm_ame"
+        }
+    }
+}
 TexturePack {
     key = "shootthelookstothemoon",
-    textures = { "shootthelookstothemoon_lttm" },
+    textures = { "shootthelookstothemoon_lttm", "shootthelookstothemoon_echo" },
     loc_txt = {
         name = "Shoot the Looks to the Moon",
         text = { "Replaces Shoot the Moon", "with Looks to the Moon" }
@@ -21,5 +34,9 @@ TexturePack {
 }
 Malverk.badges.lttm_ame = function(self, card, badges)
     badges[#badges + 1] = create_badge('Artist: Ame', G.C.PURPLE, G.C.WHITE, 0.7)
-    badges[#badges + 1] = create_badge('Common', G.C.BLUE, nil, 1.2)
+    if self.key == 'j_shoot_the_moon' then
+        badges[#badges + 1] = create_badge('Common', G.C.RARITY.Common, nil, 1.2)
+    else
+        badges[#badges + 1] = create_badge('Uncommon', G.C.RARITY.Uncommon, nil, 1.2)
+    end
 end
